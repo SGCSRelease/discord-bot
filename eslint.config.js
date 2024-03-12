00c88e5bfcfdc246ed64ea4a734a539ac2c4b71c
RangHo Lee <hello@rangho.me>
2024-03-12 12:05:39 +0900

    chore: add linter configuration

---
+++ eslint.config.js
@@ -0,0 +1,9 @@
+import eslint from '@eslint/js';
+import tslint from 'typescript-eslint';
+
+const config = tslint.config(
+    eslint.configs.recommended,
+    ...tslint.configs.recommended
+);
+
+export default config;
