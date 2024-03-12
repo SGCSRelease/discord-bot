1e453602f7d70066e47f9b966717367791401bde
RangHo Lee <hello@rangho.me>
2024-03-12 12:12:13 +0900

    chore: add prettier

---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+const config = {
+  "arrowParens": "avoid",
+  "bracketSameLine": false,
+  "bracketSpacing": true,
+  "endOfLine": "lf",
+  "printWidth": 80,
+  "proseWrap": "preserve",
+  "semi": true,
+  "singleQuote": true,
+  "tabWidth": 2,
+  "trailingComma": "all",
+  "useTabs": false
+};
+
+export default config;
